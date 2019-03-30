5da6cf92fc8b3c5790a8ab9cbcf5d637bdb4b027
sridharjajoo <sridharjajoo@gmail.com>
2019-03-31 05:20:10 +0530

    Add navigation and distress mode

---
+++ android-maps-directions-master/app/src/main/java/com/thecodecity/mapsdirection/MapActivity.java
@@ -0,0 +1,124 @@
+package com.thecodecity.mapsdirection;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.thecodecity.mapsdirection.directionhelpers.FetchURL;
+import com.thecodecity.mapsdirection.directionhelpers.TaskLoadedCallback;
+
+/**
+ * Created by Vishal on 10/20/2018.
+ */
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback, LocationListener {
+    private GoogleMap mMap;
+    private MarkerOptions place1, place2;
+    Button getDirection;
+    private Polyline currentPolyline;
+    private LocationManager locationManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+        getDirection = findViewById(R.id.btnGetDirection);
+        getDirection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new FetchURL(MapActivity.this).execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving");
+            }
+        });
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        //27.658143,85.3199503
+        //27.667491,85.3208583
+        place1 = new MarkerOptions().position(new LatLng(25.658143, 81.3199503)).title("Location 1");
+        place2 = new MarkerOptions().position(new LatLng(25.425208, 81.887764)).title("Location 2");
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapNearBy);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Log.d("mylog", "Added Markers");
+        mMap.addMarker(place1).setDraggable(true);
+        mMap.addMarker(place2).setDraggable(true);
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+            }
+        });
+    }
+
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+        return url;
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null)
+            currentPolyline.remove();
+        currentPolyline = mMap.addPolyline((PolylineOptions) values[0]);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i("MainActivity.class", "onLocationChanged: " + location.getLatitude() + " " + location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
